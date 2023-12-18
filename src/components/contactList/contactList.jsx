911fedeb8b57c238340b3a1dd8b22518791d24dc
Artem <krylov.artem.1995@gmail.com>
2023-12-18 21:03:57 +0100

    back to first searcbar

--- src/components/contactList/contactList.jsx
+++ src/components/contactList/contactList.jsx
@@ -1,33 +1,23 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import css from './contact.module.css';
 import PropTypes from 'prop-types';
 
 import { ContactListItem } from 'components/contactListItem/contactListItem';
 
-export const ContactList = ({ filtredContacts, onDeleteContact }) => {
-  const handleDeleteContact = useCallback(
-    id => {
-      onDeleteContact(id);
-    },
-    [onDeleteContact]
-  );
-  console.log('Filtered Contacts:', filtredContacts);
-  return (
-    <div className={css.List}>
-      {filtredContacts.map(contact => (
-        <ContactListItem
-          key={contact.id}
-          id={contact.id}
-          name={contact.name}
-          number={contact.number}
-          onDeleteContact={handleDeleteContact}
-        />
-      ))}
-    </div>
-  );
-};
+export const ContactList = ({ filtredContacts, onDeleteContact }) => (
+  <div className={css.List}>
+    {filtredContacts.map(contacts => (
+      <ContactListItem
+        key={contacts.id}
+        id={contacts.id}
+        name={contacts.name}
+        number={contacts.number}
+        onDeleteContact={onDeleteContact}
+      />
+    ))}
+  </div>
+);
 
 ContactList.propTypes = {
   filtredContacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired,
 };
